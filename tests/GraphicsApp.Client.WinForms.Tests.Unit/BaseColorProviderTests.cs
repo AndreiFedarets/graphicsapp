028b56d927f3dd47245468b309a804186d3780a1
Andrei Fedarets <andrei.fedarets@hotmail.com>
2023-07-15 02:20:14 +0300

    Adding more tests

---
+++ tests/GraphicsApp.Client.WinForms.Tests.Unit/BaseColorProviderTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+
+namespace GraphicsApp.Client.WinForms.Tests.Unit
+{
+    public class BaseColorProviderTests
+    {
+        [Fact]
+        public void BaseColor_ReturnsInitialColor_ByDefault()
+        {
+            // arrange
+            var colorProvider = new BaseColorProvider(Color.Green);
+
+            // assert
+            Assert.Equal(Color.Green, colorProvider.BaseColor);
+        }
+
+        [Fact]
+        public void BaseColor_ReturnsNewColor_AfterChange()
+        {
+            // arrange
+            var colorProvider = new BaseColorProvider(Color.Green);
+
+            // act
+            colorProvider.SetBaseColor(Color.Red);
+
+            // assert
+            Assert.Equal(Color.Red, colorProvider.BaseColor);
+        }
+
+        [Fact]
+        public void SetBaseColor_Raises_BaseColorChangedEvent()
+        {
+            // arrange
+            var colorProvider = new BaseColorProvider(Color.Green);
+            Color colorAtEvent = Color.Green;
+            colorProvider.BaseColorChanged += (sender, args) =>
+            {
+                colorAtEvent = colorProvider.BaseColor;
+            };
+
+            // act
+            colorProvider.SetBaseColor(Color.Red);
+
+            // assert
+            Assert.Equal(Color.Red, colorAtEvent);
+        }
+    }
+}
